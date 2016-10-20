508dd63a2c6fb899284f1acfb8a2644d37516fea
Manuel Vicente Vivo <manuel.vivo@whitelynx.co.uk>
2016-10-20 04:58:09 +0100

    Adds an AnimatedVectorDrawable

--- app/src/main/java/com/manuelvicnt/iconanimatedvectordrawable/MainActivity.java
+++ app/src/main/java/com/manuelvicnt/iconanimatedvectordrawable/MainActivity.java
@@ -1,13 +1,23 @@
 package com.manuelvicnt.iconanimatedvectordrawable;
 
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView exchangeDealLogoImageView = (ImageView) findViewById(R.id.logo);
+        Drawable drawable = exchangeDealLogoImageView.getDrawable();
+        if (drawable instanceof Animatable) {
+            ((Animatable) drawable).start();
+        }
     }
 }
